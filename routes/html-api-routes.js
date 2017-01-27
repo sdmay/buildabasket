a48afb773a8e9ccaed76580142f685daf12fac09
sdmay10 <sdmay10@gmail.com>
2017-01-27 15:58:25 -0500

    Handlebars

--- routes/html-api-routes.js
+++ routes/html-api-routes.js
@@ -4,7 +4,7 @@ var path = require("path");
 module.exports = function (app) {
 
     app.get("/", function (req, res) {
-        res.sendFile(path.join(__dirname + "/../public/index.html"));
+        res.redirect("/complete")
     });
 
     app.get("/contact", function (req, res) {
