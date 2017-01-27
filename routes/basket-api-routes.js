0dfca3c928888b4c7088f1f20990e2af176bf984
sdmay10 <sdmay10@gmail.com>
2017-01-27 17:12:35 -0500

    Contact Handle Bars

--- routes/basket-api-routes.js
+++ routes/basket-api-routes.js
@@ -4,7 +4,7 @@ var db = require("../models");
 // =============================================================
 module.exports = function (app) {
 
-    app.get("/", function (req, res) {
+    app.get("/complete", function (req, res) {
 
         db.CompleteBasket.findAll({}).then(function (dbCompleteBasket) {
             // console.log(dbCompleteBasket);
