e4775807beae62851735c6a3aa5c5bb7f6c52908
kfpedraza <kfpedraza@gmail.com>
2017-02-03 15:27:12 -0500

    Bulk of CSS styles for all pages

--- public/assets/js/checkoutPost.js
+++ public/assets/js/checkoutPost.js
@@ -50,3 +50,56 @@ $("#closeModal").click(function(){
 
 });
 
+// JotForm button ID
+   $("#input_18").click(function(){
+        console.log("CLICK")
+
+simpleCart.each(function (item, x) {
+    var item_name = (item.get('name'));
+    var item_name1 = (item.get('name'));
+    var quantity = (item.get('quantity'));
+    var quantity1 = (item.get('quantity'));
+    var total = (item.get('price'));
+    console.log(item);
+
+    var lessProduct = {
+                item_name:  item_name,
+                quantity: quantity,
+                total: total,
+                userId: 1
+            
+    };
+    console.log(lessProduct)
+    $.post("/api/lessproduct", lessProduct)
+    .done(function(data) {
+        // Log the data we found
+       
+        console.log("LESSPRODUCT");
+        console.log(data)
+        
+    // On success, run the following code
+         
+    });
+    
+});
+$("#myModal").modal("toggle");
+    });
+
+    
+  
+ $(".addToCart").click(function(){
+ $("#completeModal").modal("toggle");
+ });
+ 
+
+
+
+ 
+
+
+$("#closeModal").click(function(){
+    simpleCart_quantity = 0;
+    window.location.href='/complete';
+
+});
+
